31fdde8c5688ffff3e541af7d287e51aa687fbfc
Bread <90331269+BreadCatto@users.noreply.github.com>
2022-05-29 14:37:57 +0530

    Update p2p.sh

--- p2p.sh
+++ p2p.sh
@@ -3,10 +3,10 @@ echo "P2P INSTALL SCRIPT"
 echo "* Enter the email address: "
 read email
 echo "DOWNLOADING P2P FILES"
-wget https://updates.peer2profit.io/p2pclient_0.56_amd64.deb
+wget https://updates.peer2profit.app/p2pclient_0.60_amd64.deb
 echo "done."
 echo "UNPACKING THE FILES"
-dpkg -i p2pclient_0.56_amd64.deb
+dpkg -i p2pclient_0.60_amd64.deb
 echo "LOGGING IN"
 p2pclient --login $email
 echo " "done": ")! For help, type"
